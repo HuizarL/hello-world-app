d8c047ad3a4744abbfc02876f37e4c39a4a8caf1
HuizarL <luishuizar10@gmail.com>
2024-10-07 16:13:31 -0700

    Default Push

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
+      <h1>Hello World!</h1>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -21,6 +22,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Hello World! How are you guys? Words Words</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
